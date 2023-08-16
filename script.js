7fe806b4727e228c9134de8c5eaf39e1642a7a58
vpraninskiene <valentina.praninskiene@gmail.com>
2023-08-16 23:12:20 +0300

    init

---
+++ script.js
@@ -0,0 +1,34 @@
+/* ------------------------------ TASK 5 -----------------------------------
+Turimas "users" masyvas. 
+
+Parašykite funckijas, kurios atlikas nurodytas užduotis
+1. funkcija "filterDogOwers" - kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins "users", kurie turi augintinius.
+2. funkcija "filterAdults" - kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins masyvą su "users", kurie yra pilnamečiai.
+-------------------------------------------------------------------------- */
+
+const users = [
+  { id: '1', name: 'John Smith', age: 20, hasDog: true },
+  { id: '2', name: 'Ann Smith', age: 24, hasDog: false },
+  { id: '3', name: 'Tom Jones', age: 31, hasDog: true },
+  { id: '4', name: 'Rose Peterson', age: 17, hasDog: false },
+  { id: '5', name: 'Alex John', age: 25, hasDog: true },
+  { id: '6', name: 'Ronald Jones', age: 63, hasDog: true },
+  { id: '7', name: 'Elton Smith', age: 16, hasDog: true },
+  { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
+  { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
+];
+
+let dogOwners = users.filter(function (user) {
+  return user.hasDog = true;
+}
+);
+console.log(dogOwners);
+
+let adultUser = users.filter(function (user) {
+  return user.age >= 18
+    
+}
+);
+console.log(adultUser);
